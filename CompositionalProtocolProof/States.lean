066b798badc5cf370d4f7f44d604de4bb618eb34
An Qi Zhang <an.qi.zhang@utah.edu>
2025-05-22 23:45:27 -0600

    finish Permissions lt and le defs, to get state.lt def to work. TODO: finish instLT for State.

--- CompositionalProtocolProof/States.lean
+++ CompositionalProtocolProof/States.lean
@@ -22,26 +22,54 @@ def ReadWritePermissions.lt : ReadWritePermissions → ReadWritePermissions → 
 
 instance ReadWritePermissions.instLT : (LT ReadWritePermissions) := {lt := ReadWritePermissions.lt}
 
-#check ReadWritePermissions.r < ReadWritePermissions.wr
-
-set_option diagnostics true
-
 instance ReadWritePermissions.instDecidableLt (rw₁ rw₂ : ReadWritePermissions) : (Decidable (rw₁ < rw₂)) :=
   inferInstanceAs (Decidable (rw₁.nat < rw₂.nat))
-
-#eval ReadWritePermissions.r < ReadWritePermissions.wr
 
 def ReadWritePermissions.le : ReadWritePermissions → ReadWritePermissions → Prop
 | rw₁, rw₂ => rw₁ < rw₂ ∨ rw₁ = rw₂
 
 instance ReadWritePermissions.instLE : (LE ReadWritePermissions) := {le := ReadWritePermissions.le}
 
+instance ReadWritePermissions.instDecidableLe (rw₁ rw₂ : ReadWritePermissions) : (Decidable (rw₁ ≤ rw₂)) :=
+  inferInstanceAs (Decidable (rw₁.nat < rw₂.nat ∨ rw₁ = rw₂))
 
 /--
 Permissions.
 A structure's state may have WR, R, or no permissions
 -/
-abbrev Permissions := Option ReadWritePermissions
+def Permissions := Option ReadWritePermissions
+
+def Permissions.nat : Permissions → Nat
+| none => 0
+| some rw => rw.nat + 1
+
+def Permissions.lt : Permissions → Permissions → Prop
+| p₁, p₂ => p₁.nat < p₂.nat
+  /- How to make Permissions accept the definition below for instDecidableLt? -/
+  -- match p₁, p₂ with
+  -- | none, none => false
+  -- | some _, none => false
+  -- | none, some _ => true
+  -- | some rw₁, some rw₂ => rw₁ < rw₂
+
+instance Permissions.instLT : (LT Permissions) := {lt := Permissions.lt}
+
+instance Permissions.instDecidableLt (p₁ p₂ : Permissions) : (Decidable (p₁ < p₂)) :=
+  inferInstanceAs (Decidable (p₁.nat < p₂.nat))
+
+def Permissions.le : Permissions → Permissions → Prop
+| p₁, p₂ => p₁.nat ≤ p₂.nat
+  /- How to make Permissions accept the definition below for instDecidableLt? -/
+  -- match p₁, p₂ with
+  -- | none, none => false
+  -- | some _, none => false
+  -- | none, some _ => true
+  -- | some rw₁, some rw₂ => rw₁ < rw₂
+
+instance Permissions.instLE : (LE Permissions) := {le := Permissions.le}
+
+instance Permissions.instDecidableLe (p₁ p₂ : Permissions) : (Decidable (p₁ ≤ p₂)) :=
+  inferInstanceAs (Decidable (p₁.nat ≤ p₂.nat))
 
 structure State where
   p : Permissions
