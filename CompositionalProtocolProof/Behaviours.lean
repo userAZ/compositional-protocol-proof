d78a3df8400d19cef811c4013a05e7d947bd1b56
An Qi Zhang <an.qi.zhang@utah.edu>
2025-05-28 19:25:05 -0600

    Add (sorry - not implemented) Behaviour.Events and Behaviour.ImmediatePredecessor

--- CompositionalProtocolProof/Behaviours.lean
+++ CompositionalProtocolProof/Behaviours.lean
@@ -1,4 +1,21 @@
 import CompositionalProtocolProof.EventRelations
 
 structure Behaviour where
-  rels : sorry
+  -- rels : Set Prop -- Prop a bad idea
+  rels : Set EventRelation
+
+/- Think if i need this for defining immediate predecessor or not.-/
+def Behaviour.Events : Behaviour → Set Event
+| b =>
+  let sets := b.rels.image EventRelation.Events;
+  sorry
+
+def Behaviour.ImmediatePredecessor : Behaviour → Event → Option Event
+| b, e => sorry
+  /- find e', where
+    1. e' is Ordered with e.
+    2. there is no e'' where e' is Ordered with e'', and e'' is Ordered with e.
+  -/
+  -- start with defining all events in b
+  -- then define all events in b that are Ordered with e (can be related transitively (indirectly) through encapsulates, or ordered)
+  -- not sure how to proceed
