3d68c40c2d26209522611bd5a555d8fb101f07f3
An Qi Zhang <an.qi.zhang@utah.edu>
2025-05-30 13:51:55 -0600

    add working MWE for getting a set from another set with Nats

--- CompositionalProtocolProof/MWEs/MWENats.lean
+++ CompositionalProtocolProof/MWEs/MWENats.lean
@@ -3,14 +3,25 @@ import Mathlib
 namespace MWE
 
 inductive Nats
-| two : Nat → Nat → Nats
+| order : Nat → Nat → Nats
 | one : Nat → Nats
 
 abbrev SetNats := Set Nats
 
-def Nats.getNats : Nats → Set Nat
-| .two n₁ n₂ => {n₁, n₂}
-| .one n => {n}
+def Nats.before : Nats → Nat → Prop
+| ns, n =>
+  match ns with
+  | order _ n₂ => n = n₂
+  | one _ => false
 
-def SetNats.SetNat : SetNats → Set Nat
-| sns => sns.image Nats.getNats
+/- How does one express this in lean? -/
+def SetNats.before (x : SetNats) (n : Nat) : {y : SetNats // ∀ m ∈ y, m.before n ∧ m ∈ x} :=
+⟨
+  {nats ∈ x | nats.before n},
+  by
+    simp
+    intro m m_in_x m_before_n
+    apply And.intro
+    case left => exact m_before_n
+    case right => exact m_in_x
+⟩
