0942722c4e4539ab73856410c836103f2c10d132
Andrés Goens <andres@goens.org>
2025-06-03 09:32:38 +0200

    add proof stub for Lemma 1

--- CompositionalProtocolProof/Behaviours.lean
+++ CompositionalProtocolProof/Behaviours.lean
@@ -1,4 +1,5 @@
 import CompositionalProtocolProof.EventRelations
+import Canonical
 
 structure Behaviour where
   es : Set Event
@@ -36,6 +37,29 @@ def Behaviour.ImmBottomPredecessors : Behaviour → Event → Set Event
 
 def Set.IsSingleton {α : Type} (s : Set α) : Prop := ∃ e, {e} = s
 
+
+lemma Behaviour.immediate_bottom_predecessor_unique (b : Behaviour) (e_succ : Event) (hsucc_in_b : e_succ ∈ b.es)
+  (e_pred₁ e_pred₂ : Event) (he₁_b : b.IsImmediateBottomPred e_pred₁ e_succ) (he₂_b : b.IsImmediateBottomPred e_pred₁ e_succ) :
+  e_pred₁ = e_pred₂ := by
+    sorry -- this is the "multiple" case in Lemma 1.
+
+lemma Set.nonempty_unique_is_singleton {α} (s : Set α) (h_nonempty : Nonempty s)
+  (h_unique : ∀ (a b : α),  a ∈ s → b ∈ s → a = b) : s.IsSingleton := by
+  have ⟨a, ha⟩ := h_nonempty
+  exists a
+  apply Set.ext
+  intro x
+  constructor
+  · case mp =>
+    intro hxa
+    exact -- canonical
+      Eq.rec (motive := fun a_1 t ↦ s a_1)
+        (Nonempty.rec (motive := fun t ↦ s a) (fun val ↦ ha) h_nonempty)
+        (Eq.rec (motive := fun a t ↦ a = x) (Eq.refl x) hxa)
+  · case mpr =>
+    intro hxs
+    exact h_unique x a hxs ha
+
 -- NOTE: Remember to use OrderedCacheEvents and OrderedDirectoryEvents at some point.
 /-- Lemma 1 from the Doc.
 The set of Immediate Bottom Predecessors is Empty or Unique. (without the φ on the predecessor yet.)
@@ -44,8 +68,13 @@ lemma Behaviour.immediate_bottom_predecessor_empty_or_unique (b : Behaviour) (e_
   let imm_bottom_preds := b.ImmBottomPredecessors e_succ; imm_bottom_preds = ∅ ∨ imm_bottom_preds.IsSingleton := by
   intro imm_bottom_preds
   -- unfold ImmBottomPredecessors at imm_bottom_preds
-  cases b
-  case mk es =>
-    -- cases es
-    -- unfold ImmBottomPredecessors at imm_bottom_preds
-  sorry
+  by_cases (imm_bottom_preds = ∅)
+  · case pos h_empty => exact Or.inl h_empty
+  · case neg h_nonempty =>
+    have h_nonempty' := Set.nonempty_iff_ne_empty'.mpr h_nonempty
+    have h_unique : ∀ (e₁ e₂ : Event), e₁ ∈ imm_bottom_preds → e₂ ∈ imm_bottom_preds → e₁ = e₂ := by
+      intro e₁ e₂ he₁ he₂
+      apply Behaviour.immediate_bottom_predecessor_unique b e_succ hsucc_in_b e₁ e₂
+      exact And.right he₁
+      exact And.right he₁
+    exact Or.inr (Set.nonempty_unique_is_singleton imm_bottom_preds h_nonempty' h_unique)
