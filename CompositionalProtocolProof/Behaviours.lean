4a459c0293efc9655e6e9cb7daaa8a812ca5114f
An Qi Zhang <an.qi.zhang@utah.edu>
2025-06-03 19:49:38 -0600

    partially fill in Lemma . TODO: fill in last few steps in DirectoryEvent case, then do the same to the CacheEvent case

--- CompositionalProtocolProof/Behaviours.lean
+++ CompositionalProtocolProof/Behaviours.lean
@@ -37,6 +37,36 @@ def Behaviour.ImmBottomPredecessors : Behaviour → Event → Set Event
 
 def Set.IsSingleton {α : Type} (s : Set α) : Prop := ∃ e, {e} = s
 
+/-
+lemma Set.e_in_s_nonempty {α : Type} (s : Set α) (e : α) (he_in_s : e ∈ s) : Nonempty s := by
+  exists e
+
+lemma Set.nonempty_is_not_empty {α : Type} (s : Set α) : Nonempty s → ¬s = ∅ := by
+  intro h_nonempty
+  apply Set.nonempty_iff_ne_empty'.mp
+  exact h_nonempty
+-/
+
+lemma Event.same_address_reflexive {e₁ e₂ e₃ : Event} : e₁.SameAddress e₃ → e₂.SameAddress e₃ → e₁.SameAddress e₂ := by
+  unfold SameAddress
+  unfold CacheEvent.SameAddress; unfold DirectoryEvent.SameAddress
+  unfold SameStructureRelation
+  simp
+  intro he₁_sa_e₃ he₂_sa_e₃
+  match he₁ : e₁, he₂ : e₂, he₃ : e₃ with
+  | .cacheEvent ce₁, .cacheEvent ce₂, .cacheEvent ce₃ => simp_all
+  | .directoryEvent de₁, .directoryEvent de₂, .directoryEvent de₃ => simp_all
+  | .cacheEvent ce₁, .cacheEvent ce₂, .directoryEvent de => contradiction
+  | .cacheEvent ce₁, .directoryEvent de, .cacheEvent ce₃ => contradiction
+  | .directoryEvent de, .cacheEvent ce₂, .cacheEvent ce => contradiction
+  | .directoryEvent de₁, .directoryEvent de₂, .cacheEvent ce => contradiction
+  | .directoryEvent de₁, .cacheEvent ce, .directoryEvent de₃ => contradiction
+  | .cacheEvent ce, .directoryEvent de₂, .directoryEvent de₃ => contradiction
+
+
+  -- | .directoryEvent de, .cacheEvent ce,  => sorry
+  -- | .cacheEvent ce, .directoryEvent de => sorry
+
 
 structure OrderedAddressEvents where
   dir_ordered : ∀ (e₁ e₂ : DirectoryEvent), OrderedDirectoryEvents e₁ e₂
@@ -47,7 +77,67 @@ lemma Behaviour.immediate_bottom_predecessor_unique (b : Behaviour) (e_succ : Ev
   (e_pred₁ e_pred₂ : Event) (haddress_ordered : OrderedAddressEvents)
   (he₁_b : b.IsImmediateBottomPred e_pred₁ e_succ) (he₂_b : b.IsImmediateBottomPred e_pred₂ e_succ) :
   e_pred₁ = e_pred₂ := by
-    sorry -- this is the "multiple" case in Lemma 1.
+    -- this is the "multiple" case in Lemma 1.
+    /- By Ordered Cache Events and Ordered Directory Events,
+    if e_pred₁ and e_pred₂ are different events, then they are ordered, and contradict he₁_b or he₂_b.
+    By contradiction, e_pred₁ and e_pred₂ are the same event. -/
+    by_contra h_e_pred_diff
+    match h_pred₁ : e_pred₁, h_pred₂ : e_pred₂ with
+    | .directoryEvent de₁, .directoryEvent de₂ =>
+      -- Use OrderedDirectoryEvents to show de₁ and de₂ are ordered → Contradiction.
+      have de₁_de₂_ordered_prop := haddress_ordered.dir_ordered de₁ de₂
+      have e₁_same_addr_e_succ := he₁_b.isImmPred.sameAddress
+      have e₂_same_addr_e_succ := he₂_b.isImmPred.sameAddress
+      have e₁_same_addr_e₂ := Event.same_address_reflexive e₁_same_addr_e_succ e₂_same_addr_e_succ
+      have de₁_de₂_ordered := de₁_de₂_ordered_prop e₁_same_addr_e₂
+
+      cases de₁_de₂_ordered
+      . case inl h_de₁_o_de₂ =>
+        have he₁_is_de₁ : e_pred₁.fromDirectoryEvent de₁ := by
+          unfold Event.fromDirectoryEvent
+          simp [h_pred₁]
+        have he₂_is_de₂ : e_pred₂.fromDirectoryEvent de₂ := by
+          unfold Event.fromDirectoryEvent
+          simp [h_pred₂]
+        have e₁_o_e₂ := DirectoryEvent.ordered_events he₁_is_de₁ he₂_is_de₂ h_de₁_o_de₂
+        /- Now use the definition of he₁_b and he₂_b to state that there is no intermediately ordered
+        event e₃. But we have e₁ and e₂ (pred) ordered, and both are ordered before e_succ → Contradiction -/
+        -- NOTE: when able to, clean up proof.
+        sorry
+      . case inr h_de₂_o_de₁ =>
+        have he₁_is_de₁ : e_pred₁.fromDirectoryEvent de₁ := by
+          unfold Event.fromDirectoryEvent
+          simp [h_pred₁]
+        have he₂_is_de₂ : e_pred₂.fromDirectoryEvent de₂ := by
+          unfold Event.fromDirectoryEvent
+          simp [h_pred₂]
+        have e₂_o_e₁ := DirectoryEvent.ordered_events he₂_is_de₂ he₁_is_de₁ h_de₂_o_de₁
+        sorry
+    | .cacheEvent ce₁, .cacheEvent ce₂ => sorry
+    | .directoryEvent de, .cacheEvent ce =>
+      have h_e_succ_is_dir   := he₁_b.isImmPred.sameStructure
+      have h_e_succ_is_cache := he₂_b.isImmPred.sameStructure
+
+      unfold Event.SameStructure at h_e_succ_is_dir h_e_succ_is_cache
+      unfold Event.SameStructureRelation at h_e_succ_is_dir h_e_succ_is_cache
+      simp at h_e_succ_is_dir h_e_succ_is_cache
+      unfold CacheEvent.SameCache at h_e_succ_is_cache
+
+      match e_succ with
+      | .directoryEvent de_succ => contradiction
+      | .cacheEvent ce_succ => contradiction
+    | .cacheEvent ce, .directoryEvent de =>
+      have h_e_succ_is_cache := he₁_b.isImmPred.sameStructure
+      have h_e_succ_is_dir   := he₂_b.isImmPred.sameStructure
+
+      unfold Event.SameStructure at h_e_succ_is_dir h_e_succ_is_cache
+      unfold Event.SameStructureRelation at h_e_succ_is_dir h_e_succ_is_cache
+      simp at h_e_succ_is_dir h_e_succ_is_cache
+      unfold CacheEvent.SameCache at h_e_succ_is_cache
+
+      match e_succ with
+      | .directoryEvent de_succ => contradiction
+      | .cacheEvent ce_succ => contradiction
 
 lemma Set.nonempty_unique_is_singleton {α} (s : Set α) (h_nonempty : Nonempty s)
   (h_unique : ∀ (a b : α),  a ∈ s → b ∈ s → a = b) : s.IsSingleton := by
