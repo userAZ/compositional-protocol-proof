52cb892308fe06c4ebbe42e3ac1b73e6567a175e
An Qi Zhang <an.qi.zhang@utah.edu>
2025-06-15 18:30:18 -0600

    add n to types that use Fin Nat some where

--- CompositionalProtocolProof/BehaviourRelations.lean
+++ CompositionalProtocolProof/BehaviourRelations.lean
@@ -1,5 +1,7 @@
 import CompositionalProtocolProof.Behaviours
 
+variable (n : Nat)
+
 /-
 structure Request.IsValid (r : Request) where
   non_coherent : r.NoSCNonCoherent := by simp
@@ -9,37 +11,35 @@ structure Request.IsValid (r : Request) where
   no_cwr : r.NoCoherentWeakRead := by simp
 -/
 
-noncomputable def Behaviour.reqToDirOfRequestEvent (b : Behaviour) (e_req : Event) (init : EntryState) : ValidRequest :=
-  let state_before := b.stateBefore e_req init
+noncomputable def Behaviour.reqToDirOfRequestEvent (b : Behaviour n) (e_req : Event n) (init : EntryState n) : ValidRequest :=
+  let state_before := b.stateBefore n e_req init
   match e_req.req.val, state_before.cache with
   | ⟨.w, false, _⟩, I => ⟨⟨.r, false, .Weak⟩, {non_coherent := by simp, no_write_acq := by simp, no_read_rel := by simp, no_cacq := by simp, no_cwr := by simp}⟩
   | ⟨.r, false, .Acq⟩, Vd => ⟨⟨.w, false, .Weak⟩, {non_coherent := by simp, no_write_acq := by simp, no_read_rel := by simp, no_cacq := by simp, no_cwr := by simp}⟩
   | _, _ => e_req.req
 
 /-- Axiom 3. The Request field of a Directory Event corresponding to a Request Event (Cache Event). -/
-structure Behaviour.requestDirectoryEvent (b : Behaviour) (e_req e_dir : Event) (init : EntryState) : Prop where
-  reqEvent : e_dir.isDirEventOfReqEvent e_req
+structure Behaviour.requestDirectoryEvent (b : Behaviour n) (e_req e_dir : Event n) (init : EntryState n) : Prop where
+  reqEvent : e_dir.isDirEventOfReqEvent n e_req
   sameAddr : e_req.addr = e_dir.addr
-  dirReq :  e_dir.req = b.reqToDirOfRequestEvent e_req init -- from analysis on e_req and the state it's made on
-  dirState : e_dir.isDirEventOfDirState (b.stateAfter e_dir init).directory
+  dirReq :  e_dir.req = b.reqToDirOfRequestEvent n e_req init -- from analysis on e_req and the state it's made on
+  dirState : e_dir.isDirEventOfDirState n (b.stateAfter n e_dir init).directory
 
-structure Behaviour.reqEncapsulatesDirEvent' (b : Behaviour) (e_req e_dir : Event) (init : EntryState) : Prop where
-  reqEncapDir : e_req.Encapsulates e_dir
-  dirCorresponds : b.requestDirectoryEvent e_req e_dir init
+structure Behaviour.reqEncapsulatesDirEvent' (b : Behaviour n) (e_req e_dir : Event n) (init : EntryState n) : Prop where
+  reqEncapDir : e_req.Encapsulates n e_dir
+  dirCorresponds : b.requestDirectoryEvent n e_req e_dir init
   dirInB : e_dir ∈ b.es
   reqInB : e_req ∈ b.es
 
-structure Behaviour.reqEncapCorrespondingDirEvent (b : Behaviour) (e_req : Event) (init : EntryState) : Prop where
-  reqEncapCorrDir : ∃ e_dir ∈ b.es, b.reqEncapsulatesDirEvent' e_req e_dir init
+structure Behaviour.reqEncapCorrespondingDirEvent (b : Behaviour n) (e_req : Event n) (init : EntryState n) : Prop where
+  reqEncapCorrDir : ∃ e_dir ∈ b.es, b.reqEncapsulatesDirEvent' n e_req e_dir init
 
-/-- Axiom 4. Acquire invalidates other Vc cache entries after it's directory access. -/
-structure Behaviour.acquireInvalidates (b : Behaviour) (e_req e_dir : Event) : Prop where
+/--Axiom 4. Acquire invalidates other Vc cache entries after it's directory access. -/
+structure Behaviour.acquireInvalidates (b : Behaviour n) (e_req e_dir : Event n) : Prop where
   isAcquire : e_req.isAcquire
-  encapDirEvent : ∀ init : EntryState, b.reqEncapCorrespondingDirEvent e_req init
-  invalOther : ∀ addr ≠ e_req.addr, ∃ e_inval ∈ b.es, e_dir.OrderedBefore e_inval ∧ e_inval.isVcInval
+  encapDirEvent : ∀ init : EntryState n, b.reqEncapCorrespondingDirEvent n e_req init
+  invalOther : ∀ addr ≠ e_req.addr, ∃ e_inval ∈ b.es, e_dir.OrderedBefore n e_inval ∧ e_inval.isVcInval
 
-/-- Axiom 5. Non Coherent Release writes back other Vd cache entries before it's directory access. -/
-structure Behaviour.ncReleaseWritesBack (b : Behaviour) (e_req e_dir : Event) : Prop where
   isRelease : e_req.isNCRelease
-  encapDirEvent : ∀ init : EntryState, b.reqEncapCorrespondingDirEvent e_req init
-  writeBackOther : ∀ addr ≠ e_req.addr, ∃ e_wb ∈ b.es, e_wb.OrderedBefore e_dir ∧ e_wb.isVdWriteBack
+  encapDirEvent : ∀ init : EntryState n, b.reqEncapCorrespondingDirEvent e_req init
+  writeBackOther : ∀ addr ≠ e_req.addr, ∃ e_wb ∈ b.es, e_wb.OrderedBefore n e_dir ∧ e_wb.isVdWriteBack
