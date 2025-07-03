4ae447f1ef5a78f55ec4d2613494b9418b2a92e6
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-02 23:01:17 -0600

    attempt example to show induction hypothesis changes with adding a hypothesis to the induction variable

---
+++ CompositionalProtocolProof/MWEs/MWEInductionProperty.lean
@@ -0,0 +1,22 @@
+import Mathlib
+
+-- structure NatBool where
+--   n : Nat
+--   b : Bool
+
+-- def NatBool.true (n : NatBool) : Prop := n.b
+-- def List.allTrue (l : List NatBool) := ∀ n ∈ l, n.true
+
+def List.leM (l : List Nat) (m : Nat) := ∀ n ∈ l, n < m
+
+def List.upToN (l : List Nat) (hle_m : l.leM m) (n' : Nat) :=
+  ∀ n ∈ (l.take ((l.idxOf n'))), n ≤ m
+
+example (l : List Nat) (m : Nat) (hle_m : l.leM m) (hl_sorted : l.Sorted Nat.le) : sorry := by
+  induction H : l with
+  | nil =>
+    sorry
+  | cons h tail ih =>
+    /- Here, I know `h` and `tail` are in list `l`, but the induction hypothesis `ih` is unusable.
+    `l = tail` is not a workable form. -/
+    sorry
