d4e04251a0ebac5a4755017dba55a2ee3d872ba4
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-16 15:35:54 -0600

    add def of a protocol

---
+++ CompositionalProtocolProof/Protocol.lean
@@ -0,0 +1,22 @@
+import CompositionalProtocolProof.BehaviourRelationProofs
+import CompositionalProtocolProof.Requests
+
+variable (n : Nat)
+
+structure Protocol where
+  pi : ProtocolInstance -- Which `cluster` is this protocol associated with
+  requests : ProtocolInterface
+  /- Axioms 4-14 -/
+  acqInvals : Behaviour.acqInvalWrapper n
+  relWritesBack : Behaviour.ncRelWriteBackWrapper n
+  whenReqAccessDir : Behaviour.axRequestAccessesDirectory n
+  vdLaterWBOrGetSW : Behaviour.vdCacheEntryWBOrGetSWLaterWrapper n
+  dirIdsOrdered : Behaviour.deidOrdered n
+  coherentWriteDowngrades : Behaviour.coherentWriteDirDowngradeOthers n
+  coherentReadDowngrades : Behaviour.coherentReadDirDowngradeOthers n
+  coherentEvictGrant : Behaviour.coherentEvictGetsGrant n
+  nonCohReqDowngrades : Behaviour.nonCoherentRequestDowngradeOthers n
+  relAcqSelfBroadcast : Behaviour.relAcqBroadcast n
+
+
+/- Want to State if a protocol has some requests. -/
