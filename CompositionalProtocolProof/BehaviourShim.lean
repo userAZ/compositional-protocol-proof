b901384688f85070113b4431fe6bcc5e0cfd8a4c
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-15 21:12:11 -0600

    add Shim axiom 15

--- CompositionalProtocolProof/BehaviourShim.lean
+++ CompositionalProtocolProof/BehaviourShim.lean
@@ -28,8 +28,23 @@ def Event.reqAtCorrespondingGCacheOfCDir (e_greq e_dir : Event n) : Prop :=
 
 
 /-- Def 2.43: Constraints of the Global Cache Event corresponding to a Cluster Directory Event. -/
-structure Behaviour.globalCacheEventOfClusterDir (b : Behaviour n) (e_greq e_dir : Event n) where
+structure Event.globalCacheEventOfClusterDir (e_greq e_dir : Event n) where
   reqAtCache : e_greq.isCacheEvent
   dirAtDir : e_dir.isDirectoryEvent
   gReq : e_greq.reqAtCorrespondingGCacheOfCDir n e_dir -- Global Cache Request corresponds to e_dir's cluster
   matchingOp : e_greq.req = ⟨⟨e_dir.req.val.rw, true, .SC⟩, by simp[Request.IsValid']⟩
+
+def Event.isClusterDir (e_dir : Event n) : Prop := match e_dir with
+  | .directoryEvent de => match de.pInst with
+    | .cluster1 | .cluster2 => True
+    | .global => False
+  | .cacheEvent _ => False
+
+structure Event.clusterDirEncapCorrespondingGlobalCache (e_dir e_greq : Event n) : Prop where
+  encapGlobalCache : e_dir.Encapsulates n e_greq
+  gReqOfCDir : e_greq.globalCacheEventOfClusterDir n e_dir
+
+/-- Shim Axiom 15: Cluster Directory Events are translated to Request Events at the corresponding Cache in the Global Protocol. -/
+structure Behaviour.clusterDirEncapCorrespondingGlobalCache (b : Behaviour n) (e_dir : Event n) where
+  clusterDir : e_dir.isClusterDir
+  encapGlobalCache : Event.clusterDirEncapCorrespondingGlobalCache n e_greq e_dir
