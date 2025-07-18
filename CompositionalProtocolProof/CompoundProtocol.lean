730a8737224fb61940691c554e80ae9868150150
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-17 18:17:51 -0600

    define a Compound Protocol

---
+++ CompositionalProtocolProof/CompoundProtocol.lean
@@ -0,0 +1,18 @@
+import CompositionalProtocolProof.Protocol
+import CompositionalProtocolProof.BehaviourShim
+
+variable (n : Nat)
+
+structure ShimAxioms where
+  clusterToGlobal : ∀ b : Behaviour n, ∀ e_dir ∈ b, Behaviour.Shim.ClusterToGlobal n b e_dir
+  globalToCluster : ∀ b : Behaviour n, ∀ init : InitialSystemState n, ∀ p : Protocol n, ∀ e_gdown ∈ b, Behaviour.Shim.GlobalToCluster n b init p e_gdown
+
+structure CompoundProtocol where
+  global : Protocol n
+  cluster1 : Protocol n
+  cluster2 : Protocol n
+  shimAxioms : ShimAxioms n
+  globalWellFormed : global.pi = .global
+  globalSWMR : global.requests.isSWMR
+  cluster1WellFormed : cluster1.pi = .cluster1
+  cluster2WellFormed : cluster2.pi = .cluster2
