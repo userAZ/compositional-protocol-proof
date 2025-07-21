3eed6f3cd840abc7b9ee1ca505f8539f0b607230
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-21 15:17:06 -0600

    add TODO for Lemma 9

--- CompositionalProtocolProof/CompositionalProof/Lemma9CompoundLinearization.lean
+++ CompositionalProtocolProof/CompositionalProof/Lemma9CompoundLinearization.lean
@@ -1,2 +1,17 @@
 import CompositionalProtocolProof.CompoundSWMR
 import CompositionalProtocolProof.CompoundProtocol
+
+/- TODO:
+1. define an inductive stating the Compound Linearization event of a Cluster Cache Request Event
+  is defined by two cases on what the Request's Linearization event is in the cluster:
+  (a) a Cluster Directory Event `e_cdir`:
+    Either `e_cdir` has permissions or not.
+    If not:
+      Then there exists a Global Linearization Event (Cache or Directory),
+      stemming from Shim Axiom 15.
+    If it does:
+      `e_cdir` is the linearization Event.
+  (b) a Cluster Cache Event:
+    There exists a previous event that obtained permissions, and enforced (did not violate) Compound SWMR
+    (True because of Lemma 8 -- all Cluster requests do not violate Compound SWMR).
+   -/
