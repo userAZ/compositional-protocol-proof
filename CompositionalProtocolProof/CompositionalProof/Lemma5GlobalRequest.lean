10abd7d5cbd147f30f773adf9445404670fe6e82
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-21 16:43:52 -0600

    add TODO comment f or Lemma 5

--- CompositionalProtocolProof/CompositionalProof/Lemma5GlobalRequest.lean
+++ CompositionalProtocolProof/CompositionalProof/Lemma5GlobalRequest.lean
@@ -3,3 +3,7 @@ import CompositionalProtocolProof.CompoundProtocol
 
 import CompositionalProtocolProof.CompositionalProof.Lemma6GlobalWriteDowngrade
 import CompositionalProtocolProof.CompositionalProof.Lemma7GlobalReadDowngrade
+
+/- Global Coherent Request Event in Compound SWMR Initial/Current state enforces Compound SWMR.
+For any SC Write/Read Global Cache Request Event `e_gcache`, show the compound protocol is left in Compound SWMR.
+-/
