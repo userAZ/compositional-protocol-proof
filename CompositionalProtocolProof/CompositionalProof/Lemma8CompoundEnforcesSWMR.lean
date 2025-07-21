9e6eb01aa546dcb0ce276b9c6f528918f4daa08f
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-21 16:05:19 -0600

    add TODO comment for Lemma 8

--- CompositionalProtocolProof/CompositionalProof/Lemma8CompoundEnforcesSWMR.lean
+++ CompositionalProtocolProof/CompositionalProof/Lemma8CompoundEnforcesSWMR.lean
@@ -2,3 +2,23 @@ import CompositionalProtocolProof.CompoundSWMR
 import CompositionalProtocolProof.CompoundProtocol
 
 import CompositionalProtocolProof.CompositionalProof.Lemma4ClusterRequest
+
+-- ∀ Cluster Dir & Global Cache Events, CompoundSWMR holds iff ∀ Cluster Cache Event, Compound SWMR holds
+/- TODO: Show that the Prop `CompoundSWMR` holds iff for all cluster cache events, `CompoundSWMR` holds
+  1. Fwd (MP) case: Use Request and Shim Axioms to show we can go from Cluster Cache Events to `CompoundSWMR`
+  2. Reverse (MPR) case: Let there be an arbitrary ClusterCacheEvent event `e` in Behaviour `b`.
+    a. By Request and Shim axioms, `e` may:
+      i. produce additional Cluster Directory Events and Global Cache Events,
+        that by `CompoundSWMR` still enforce `CompoundSWMR`.
+      ii. If they produce none, then the system is still in Compound SWMR.
+-/
+
+-- ∀ cmpProtocol, CompoundSWMR holds
+/- TODO: State that Cluster-Cache-Events enforce Compound SWMR
+1. consider set of events at a cluster cache entry
+  a. In the empty case, Compound SWMR is maintained (assuming initial state is Compound SWMR)
+  b. In the multiple case, map the set to the equivalent list of events at the cache entry.
+2. Multiple case:
+  a. induct on the list; by Lemma 4, we know all Cache Request Events leave the protocol in Compound SWMR;
+  b. in the inductive step case `cons`, apply the induction hypothesis.
+-/
