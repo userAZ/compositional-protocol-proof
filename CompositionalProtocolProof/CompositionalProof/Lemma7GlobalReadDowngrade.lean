6afd7ba505349e53f8c78f77d02d861d27813898
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-21 16:13:49 -0600

    add Lemma 6,7 TODO comments

--- CompositionalProtocolProof/CompositionalProof/Lemma7GlobalReadDowngrade.lean
+++ CompositionalProtocolProof/CompositionalProof/Lemma7GlobalReadDowngrade.lean
@@ -1,2 +1,10 @@
 import CompositionalProtocolProof.CompoundSWMR
 import CompositionalProtocolProof.CompoundProtocol
+
+/-
+Assume the Initial State / Current State satisfies Compound SWMR.
+  (Must define a version of Compound SWMR for InitialSystemState)
+For any global MR downgrade cache event `e_gdown`:
+1. the corresponding Cluster Directory state is ≤ the state after `e_gdown`.
+2. the corresponding Cluster is in SWMR. (techinically have this by an Axiom)
+-/
