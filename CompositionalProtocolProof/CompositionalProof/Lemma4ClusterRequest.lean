11a69766e3df5515b7f9be8a358f4609968d0d56
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-22 14:03:41 -0600

    comment out old approach for Lemma 4

--- CompositionalProtocolProof/CompositionalProof/Lemma4ClusterRequest.lean
+++ CompositionalProtocolProof/CompositionalProof/Lemma4ClusterRequest.lean
@@ -21,6 +21,7 @@ structure Behaviour.globalCacheFinishBeforeUnrelated (b : Behaviour n) (init : I
   gCacheFinishBefore : e_gcache.finishesBefore n e
   unrelated : ∃ e_dir ∈ b, b.dirEventNotCorrespondingToCacheEvent n init e_dir e ∧ e_dir.Encapsulates n e_gcache
 
+/- Don't need these
 structure Behaviour.encapCorrespondingClusterDir (b : Behaviour n) (init : InitialSystemState n) (e_cdir e : Event n) : Prop where
   dirFinishBefore : e_cdir.Encapsulates n e
   unrelated : b.dirEventCorrespondingToCacheEvent n init e_cdir e
@@ -28,6 +29,7 @@ structure Behaviour.encapCorrespondingClusterDir (b : Behaviour n) (init : Initi
 structure Behaviour.globalCacheFinishBefore (b : Behaviour n) (init : InitialSystemState n) (e_gcache e : Event n) : Prop where
   gCacheFinishBefore : e_gcache.finishesBefore n e
   unrelated : ∃ e_dir ∈ b, b.encapCorrespondingClusterDir n init e_dir e ∧ e_dir.Encapsulates n e_gcache
+-/
 
 /-- Lemma 4 : A Cluster Request Event leaves a protocol in Compound SWMR. -/
 lemma Behaviour.cluster_request_enforces_compound_swmr
@@ -38,8 +40,9 @@ lemma Behaviour.cluster_request_enforces_compound_swmr
   (e : Event n) (he_in_b : e ∈ b)
   (hpred_cdir_cmp_swmr : ∀ e_cdir ∈ b, e_cdir.isClusterDir → b.clusterDirFinishBeforeUnrelated n init e_cdir e → CompoundSWMR.stateAfterClusterDirEventLeGlobalCache n b init e_cdir)
   (hpred_gcache_cmp_swmr : ∀ e_gcache ∈ b, e_gcache.isGlobalCache → b.globalCacheFinishBeforeUnrelated n init e_gcache e → CompoundSWMR.stateAfterClusterDirEventLeGlobalCache' n b init e_gcache)
-  : ∀ e_cdir ∈ b, e_cdir.isClusterDir → b.encapCorrespondingClusterDir n init e_cdir e → CompoundSWMR.stateAfterClusterDirEventLeGlobalCache n b init e_cdir
-  ∧
-  ∀ e_gcache ∈ b, e_gcache.isGlobalCache → b.globalCacheFinishBefore n init e_cdir e → CompoundSWMR.stateAfterClusterDirEventLeGlobalCache n b init e_cdir
+  : True
+  -- ∀ e_cdir ∈ b, e_cdir.isClusterDir → b.encapCorrespondingClusterDir n init e_cdir e → CompoundSWMR.stateAfterClusterDirEventLeGlobalCache n b init e_cdir
+  -- ∧
+  -- ∀ e_gcache ∈ b, e_gcache.isGlobalCache → b.globalCacheFinishBefore n init e_cdir e → CompoundSWMR.stateAfterClusterDirEventLeGlobalCache n b init e_cdir
   := by
   sorry
