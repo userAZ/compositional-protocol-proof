472247d44bfdf7e557024334ae2bbd85225d415e
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-24 14:11:13 -0600

    add skeleton for Lemma 6/7

--- CompositionalProtocolProof/CompositionalProof/Lemma6GlobalWriteDowngrade.lean
+++ CompositionalProtocolProof/CompositionalProof/Lemma6GlobalWriteDowngrade.lean
@@ -1,5 +1,8 @@
 import CompositionalProtocolProof.CompoundSWMR
 import CompositionalProtocolProof.CompoundProtocol
+import CompositionalProtocolProof.CompositionalProof.ProofBasic
+
+variable (n : Nat)
 
 /-
 Assume the Initial State / Current State satisfies Compound SWMR.
@@ -8,3 +11,29 @@ For any global SW downgrade cache event `e_gdown`:
 1. the corresponding Cluster Directory state is ≤ the state after `e_gdown`.
 2. the corresponding Cluster is in SWMR. (techinically have this by an Axiom)
 -/
+
+/-- Lemma 6/7: A global downgrade `e_gdown` leaves it's corresponding cluster directory
+in state `s` ≤ `e_gdown.MRS` -/
+lemma CompoundProtocol.globalDowngrade.satisfies_compound_swmr
+  (cmp : CompoundProtocol n)
+  (b : Behaviour n) (init : InitialSystemState n)
+  (e_gdown : Event n) (hgdown_in_b : e_gdown ∈ b)
+  (hgdown : e_gdown.isGlobalDowngrade)
+  : CompoundSWMR n b init e_gdown := by
+  apply CompoundSWMR.gCache
+  . case gcache_satisfies_cmp_swmr =>
+    simp [Behaviour.globalCacheEvent.satisfiesCompoundSWMR]
+    intro haux_is_gcache
+    constructor
+    exact haux_is_gcache
+    . case stateAfterLeGlobalCache =>
+      simp[Behaviour.dirEventStateLeGlobalCacheState']
+      /- Strategy: Show the latest event is the one corresponding to
+      lower state to I (for fwd SW) or going to S (for fwd MR).
+      Any further event requires encapsulating another Global Cache Event `e_gcache_aux`,
+      and if it encapsulates this event, and since all non-downgrade events at a cache are ordered,
+      the last event at the corresponding directory cannot get permissions higher than `e_gdown` -/
+      /- NOTE: must know the state before this `e_gdown` satisfies Compound SWMR;
+      how should I transfer the def of events before `e_creq` satisfiy Compound SWMR to `e_gdown`.
+      Maybe not needed. Let's try the proof first. -/
+  sorry
