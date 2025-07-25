2721a3a372f615018e5f1e5d30949c0039ac3bb9
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-24 21:18:02 -0600

    add helper lemmas to state the set of events that  finish immediately before a global downgrade  is singleton if we know the event that immediately finishes before

--- CompositionalProtocolProof/CompositionalProof/Lemma6GlobalWriteDowngrade.lean
+++ CompositionalProtocolProof/CompositionalProof/Lemma6GlobalWriteDowngrade.lean
@@ -31,6 +31,155 @@ def Event.globalCacheEventCid (e_greq : Event n) : Fin 2 := match e_greq with
 
 def CompoundProtocol.clusterProtocolCorrespondingToGlobalProtocol (cmp : CompoundProtocol n) (e_greq : Event n) : Protocol n :=
   cmp.globalCidToProtocol n (e_greq.globalCacheEventCid n)
+
+lemma Behaviour.same_corresponding_gcache_same_struct {b : Behaviour n} {e_cdir e_gdown e : Event n}
+  (he_cdir_satisfies : immediateFinishesBeforeAtClusterDirectory n b e_cdir e_gdown)
+  (he_is_imm_pred : immediateFinishesBeforeAtClusterDirectory n b e e_gdown)
+  : Event.struct n e = Event.struct n e_cdir := by
+      have hcdir_at_gcache := he_cdir_satisfies.finishBefore.gCacheOfCDir
+      have he_at_gcache := he_is_imm_pred.finishBefore.gCacheOfCDir
+      simp[Event.reqAtCorrespondingGCacheOfCDir] at hcdir_at_gcache he_at_gcache
+      simp[Event.protocol] at hcdir_at_gcache he_at_gcache
+      simp [Event.struct]
+      match he : e, hecdir : e_cdir, hegdown : e_gdown with
+      | .directoryEvent de, .directoryEvent de_c, .cacheEvent ce =>
+        simp
+        match hde : de.pInst, hde_c : de_c.pInst with
+        | .global, .global
+        | .cluster1, .cluster1
+        | .cluster2, .cluster2 => simp
+        | .global, .cluster1
+        | .global, .cluster2
+        | .cluster1, .cluster2
+        | .cluster2, .cluster1
+        | .cluster2, .global
+        | .cluster1, .global =>
+          match hcid : ce.cid with
+          | .proxy pi =>
+            simp [hde, hde_c, Event.reqAtGlobalCache, hcid] at hcdir_at_gcache he_at_gcache
+          | .cache pci =>
+            match pci with
+            | .globalP fin2 =>
+              simp_all [hde, hde_c, Event.reqAtGlobalCache, hcid]
+            | .cluster1 fin | .cluster2 fin =>
+              simp [hde, hde_c, Event.reqAtGlobalCache, hcid] at hcdir_at_gcache he_at_gcache
+      | .cacheEvent ce_e, .directoryEvent de_c, .cacheEvent ce_g
+      | .cacheEvent ce_e, .cacheEvent ce_c, .cacheEvent ce_g
+      | .directoryEvent de_e, .cacheEvent ce_c, .cacheEvent ce_g
+      | .cacheEvent ce_e, .directoryEvent de_c, .directoryEvent de_g
+      | .cacheEvent ce_e, .cacheEvent ce_c, .directoryEvent de_g
+      | .directoryEvent de_e, .cacheEvent ce_c, .directoryEvent de_g
+      | .directoryEvent de_e, .directoryEvent de_c, .directoryEvent de_g
+        =>
+        all_goals simp [he, hecdir, Event.reqAtGlobalCache, hegdown] at hcdir_at_gcache he_at_gcache
+        all_goals try simp
+        try
+        match hde_e : de_e.pInst, hde_c : de_c.pInst with
+        | .global, .global
+        | .cluster1, .cluster1
+        | .cluster2, .cluster2
+        | .global, .cluster1
+        | .global, .cluster2
+        | .cluster1, .cluster2
+        | .cluster2, .cluster1
+        | .cluster2, .global
+        | .cluster1, .global =>
+          all_goals simp [hde_e, hde_c, Event.reqAtGlobalCache] at hcdir_at_gcache he_at_gcache
+
+lemma Behaviour.contradiction_of_directory_event_ends_eq {de de2}
+  {he_eq_cdir_end : Event.oEnd n (Event.directoryEvent de) = Event.oEnd n (Event.directoryEvent de2) }
+  {hde_ob_cdir : DirectoryEvent.OrderedBefore n de de2}
+  : False := by
+  simp[DirectoryEvent.OrderedBefore] at hde_ob_cdir
+  have hde_before_cdir_end : de.oEnd < de2.oEnd := by
+    calc de.oEnd < de2.oStart := hde_ob_cdir
+      _ < de2.oEnd := de2.oWellFormed
+  absurd hde_before_cdir_end
+  simp[Nat.le_iff_lt_or_eq,]
+  apply Or.intro_right
+  simp[Event.oEnd] at he_eq_cdir_end
+  simp[he_eq_cdir_end]
+
+lemma Behaviour.event_immediate_finish_before_gdown_singleton
+  {b : Behaviour n} {e_cdir e_gdown : Event n} (he_cdir_in_b : e_cdir ∈ b) (he_cdir_satisfies : immediateFinishesBeforeAtClusterDirectory n b e_cdir e_gdown)
+  : {e_pred | e_pred ∈ b ∧ Behaviour.immediateFinishesBeforeAtClusterDirectory n b e_pred e_gdown} = {e_cdir} := by
+  apply Set.ext
+  intro e
+  apply Iff.intro
+  . case h.mp =>
+    simp
+    intro he_in_b he_is_imm_pred
+    by_contra he_ne_e_cdir
+    have hcdir_no_inter := he_cdir_satisfies.noIntermediate
+    simp[Behaviour.noIntermediateFinishesBeforeOfSameEntry] at hcdir_no_inter
+    have he_no_inter := he_is_imm_pred.noIntermediate
+    simp[Behaviour.noIntermediateFinishesBeforeOfSameEntry] at he_no_inter
+
+    -- start by showing `e_cdir` has no intermediate
+    have he_not_inter_cdir := hcdir_no_inter e he_in_b
+    apply he_not_inter_cdir
+    constructor
+    . case sameCidInterPred =>
+      apply Behaviour.same_corresponding_gcache_same_struct
+      . case he_cdir_satisfies => exact he_cdir_satisfies
+      . case he_is_imm_pred => exact he_is_imm_pred
+    . case sameAddr =>
+      have hcdir_addr := he_cdir_satisfies.finishBefore.finBefore.sameAddr
+      have he_addr := he_is_imm_pred.finishBefore.finBefore.sameAddr
+      simp[Event.sameAddr] at hcdir_addr he_addr
+      simp[hcdir_addr, he_addr]
+    . case interPred =>
+      by_contra hcdir_not_finish_before_e
+      simp[Event.finishesBefore] at hcdir_not_finish_before_e
+      simp[Nat.le_iff_lt_or_eq] at hcdir_not_finish_before_e
+      cases hcdir_not_finish_before_e
+      . case inl he_lt_cdir_end =>
+        have hcdir_not_inter_e := he_no_inter e_cdir he_cdir_in_b
+        apply hcdir_not_inter_e
+        constructor
+        . case sameCidInterPred =>
+          apply Eq.symm
+          apply Behaviour.same_corresponding_gcache_same_struct
+          . case he_cdir_satisfies => exact he_cdir_satisfies
+          . case he_is_imm_pred => exact he_is_imm_pred
+        . case sameAddr =>
+          have hcdir_addr := he_cdir_satisfies.finishBefore.finBefore.sameAddr
+          have he_addr := he_is_imm_pred.finishBefore.finBefore.sameAddr
+          simp[Event.sameAddr] at hcdir_addr he_addr
+          simp[hcdir_addr, he_addr]
+        . case interPred => simp[Event.finishesBefore, he_lt_cdir_end]
+        . case interSucc => simp[he_cdir_satisfies.finishBefore.finBefore.endBefore]
+      . case inr he_eq_cdir_end =>
+        -- Contradiction, all directory events are ordered.
+        match he : e, hcdir : e_cdir with
+        | .directoryEvent de, .directoryEvent de_cdir =>
+          simp[Event.oEnd] at he_eq_cdir_end
+          have hordered := b.orderedAtEntry.dir_ordered de de_cdir |>.ordered
+          simp[DirectoryEvent.Ordered] at hordered
+          cases hordered
+          . case inl hde_ob_cdir =>
+            apply Behaviour.contradiction_of_directory_event_ends_eq
+            . case he_eq_cdir_end => exact he_eq_cdir_end
+            . case hde_ob_cdir => exact hde_ob_cdir
+          . case inr hcdir_ob_de =>
+            apply Behaviour.contradiction_of_directory_event_ends_eq
+            . case he_eq_cdir_end =>
+              apply Eq.symm
+              exact he_eq_cdir_end
+            . case hde_ob_cdir => exact hcdir_ob_de
+        | .cacheEvent ce, .directoryEvent de_cdir
+        | .directoryEvent de, .cacheEvent ce_cdir
+        | .cacheEvent ce, .cacheEvent ce_cdir
+          =>
+          have hcdir_at_gcache := he_cdir_satisfies.finishBefore.gCacheOfCDir
+          have he_at_gcache := he_is_imm_pred.finishBefore.gCacheOfCDir
+          simp[Event.reqAtCorrespondingGCacheOfCDir] at hcdir_at_gcache he_at_gcache
+    . case interSucc => exact he_is_imm_pred.finishBefore.finBefore.endBefore
+  . case h.mpr =>
+    simp
+    intro he_eq_e_cdir
+    simp[he_eq_e_cdir, he_cdir_in_b]
+    exact he_cdir_satisfies
 /-- Lemma 6/7: A global downgrade `e_gdown` leaves it's corresponding cluster directory
 in state `s` ≤ `e_gdown.MRS` -/
 lemma CompoundProtocol.globalDowngrade.satisfies_compound_swmr
