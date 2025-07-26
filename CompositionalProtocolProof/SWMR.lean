81c5fc3d3fb2e50c5e5d9c60d57af86f8b920338
An Qi Zhang <an.qi.zhang@utah.edu>
2025-07-25 20:16:12 -0600

    add lemma, show that Set.toOption of a singleton set  produces

--- CompositionalProtocolProof/SWMR.lean
+++ CompositionalProtocolProof/SWMR.lean
@@ -11,6 +11,25 @@ noncomputable def Set.toOption {α} (s : Set α) : Option (α) :=
   if h : Nonempty s then some h.some
   else none
 
+lemma Set.toOption_singleton {α} (s : Set α) (hsingleton : s.IsSingleton) : ∃ e, s = {e} → s.toOption = some e := by
+  use hsingleton.choose
+  intro hs_singleton
+  simp only [toOption, Option.dite_none_right_eq_some,]
+  have hs_nonempty' : Nonempty s := by
+    simp []
+    use hsingleton.choose
+    simp[Set.eq_singleton_iff_unique_mem] at hs_singleton
+    obtain ⟨hsingle_in_s, helem_of_s⟩ := hs_singleton
+    simp[hsingle_in_s]
+  use hs_nonempty'
+  obtain ⟨_,hxs_eq_singleton⟩ := Set.eq_singleton_iff_unique_mem.mp hs_singleton
+  simp
+  apply hxs_eq_singleton
+  . case h.intro.a =>
+    apply Nonempty.some_mem
+    . case h =>
+      use hsingleton.choose
+
 noncomputable def Behaviour.eventToState (b : Behaviour n) (init : InitialSystemState n) (e? : Option (Event n)) (struct : Struct n) : State :=
   match e? with
   | .none => init.stateAtStruct n struct
