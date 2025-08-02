e3438835338bd297ecb3a2c089238c0ad75045b9
An Qi Zhang <an.qi.zhang@utah.edu>
2025-08-02 14:26:16 -0600

    move defs to Compound Protocol where they belong; and import compound Linearization event into compound protocol

--- CompositionalProtocolProof/CompoundProtocol.lean
+++ CompositionalProtocolProof/CompoundProtocol.lean
@@ -1,5 +1,6 @@
 import CompositionalProtocolProof.Protocol
 import CompositionalProtocolProof.BehaviourShim
+import CompositionalProtocolProof.CompositionalProof.CompoundLinearization
 
 variable (n : Nat)
 
@@ -14,3 +15,14 @@ structure CompoundProtocol where
   cluster2WellFormed : cluster2.pi = .cluster2
   linearizationOfEvent : ∀ b : Behaviour n, ∀ init : InitialSystemState n, ∀ e_req ∈ b,
     Behaviour.linearizationEventOfRequest n b init e_req
+  compoundLinearizationEvent : ∀ b : Behaviour n, ∀ init : InitialSystemState n, ∀ e_creq ∈ b, ∀ e_creq_lin : b.linearizationEventOfRequest n init e_creq,
+    ClusterRequestLinearizationEvent n shimAxioms b init e_creq e_creq_lin
+
+def CompoundProtocol.globalCidToProtocol (cmp : CompoundProtocol n) (g_cid : Fin 2) : Protocol n := match g_cid with
+  | 0 => cmp.cluster1
+  | 1 => cmp.cluster2
+
+def CompoundProtocol.clusterProtocolCorrespondingToGlobalProtocol (cmp : CompoundProtocol n) (e_greq : Event n) : Protocol n :=
+  cmp.globalCidToProtocol n (e_greq.globalCacheEventCid n)
+
+/- ----------------------------------------------- -/
