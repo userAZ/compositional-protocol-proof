41e18a44b97f303501079a5a5581ae67259e4402
An Qi Zhang <an.qi.zhang@utah.edu>
2025-08-02 20:43:19 -0600

    move defs for Event.protocol and Event.sameProtocol to EventRelations.file

--- CompositionalProtocolProof/BehaviourRelationDefs.lean
+++ CompositionalProtocolProof/BehaviourRelationDefs.lean
@@ -51,17 +51,6 @@ noncomputable def Behaviour.reqToDirOfRequestEvent (b : Behaviour n) (init : Ent
     let state_before := b.stateBefore n init e_req
     e_req.reqToDirOfRequestEvent n state_before.cache
 
-def Event.protocol (e_req : Event n) : ProtocolInstance := match e_req with
-  | .cacheEvent ce => match ce.cid with
-    | .proxy pi => pi
-    | .cache pci => match pci with
-      | .globalP _ => .global
-      | .cluster1 _ => .cluster1
-      | .cluster2 _ => .cluster2
-  | .directoryEvent de => de.pInst
-
-def Event.sameProtocol (e_req e_dir : Event n) : Prop := e_req.protocol = e_dir.protocol
-
 /-- Axiom 3. The Request field of a Directory Event corresponding to a Request Event (Cache Event). -/
 structure Behaviour.requestDirectoryEvent (b : Behaviour n) (init : EntryState n) (rel_wb : Bool) (e_req e_dir : Event n) : Prop where
   reqEvent : e_dir.isDirEventOfReqEvent n e_req
