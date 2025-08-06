22070b063909a78fdd6dc1bab2e6c6498d640cb2
An Qi Zhang <an.qi.zhang@utah.edu>
2025-08-05 18:16:33 -0600

    return to using VdEntry, easier to work with

--- CompositionalProtocolProof/BehaviourRelationDefs.lean
+++ CompositionalProtocolProof/BehaviourRelationDefs.lean
@@ -520,7 +520,8 @@ structure Behaviour.ncWeakReqOnVd (b : Behaviour n) (init : InitialSystemState n
 
 /-- Succeeding Request Event on Vd that accesses the Directory -/
 structure Behaviour.reqOnVdWithCorrespondingDir (b : Behaviour n) (init : InitialSystemState n) (e_req e_dir : Event n) : Prop where
-  stateBeforeAsVd : (b.stateBefore n (init.stateAt n e_req) e_req).cache = Vd
+  stateBeforeAsVd : (b.stateBefore n (init.stateAt n e_req) e_req) = VdEntry n
+  -- stateAfterNotVd : (b.stateAfter n (init.stateAt n e_req) e_req).cache ≠ Vd
   isRelAcqOrVdWB : e_req.isAcquire ∨ e_req.isNcRelease ∨ e_req.isCRelease ∨ e_req.isVdWriteBack
     ∨ e_req.isSCWrite ∨ e_req.isSCRead
   -- [NOTE]: Remebmer to use Axiom 6 to solve this.
