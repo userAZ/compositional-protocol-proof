77c3ca90982d817e56c66a35e0d4eb38059bdc2d
installer <installer@gmail.com>
2026-02-20 23:18:59 -0700

    add Event.isClusterCache and Event.isClusterDir to Event.lean

--- CompositionalProtocolProof/Events.lean
+++ CompositionalProtocolProof/Events.lean
@@ -221,9 +221,17 @@ def Event.isCacheEvent : Event n → Prop
 | .directoryEvent _ => false
 | .cacheEvent _ => true
 
+structure Event.isClusterCache (e_dir : Event n) : Prop where
+  dirAtDir : e_dir.isCacheEvent
+  dirCluster : e_dir.protocol = .cluster1 ∨ e_dir.protocol = .cluster2
+
 def Event.isDirectoryEvent : Event n → Prop
 | .directoryEvent _ => true
 | .cacheEvent _ => false
+
+structure Event.isClusterDir (e_dir : Event n) : Prop where
+  dirAtDir : e_dir.isDirectoryEvent
+  dirCluster : e_dir.protocol = .cluster1 ∨ e_dir.protocol = .cluster2
 
 def Event.isCacheEventAtCid : Event n → CacheId n → Prop
 | e, cid => match e with
