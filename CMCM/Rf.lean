24f195162f46031d988a1fb8ef904a6e34071a81
installer <installer@gmail.com>
2026-02-20 23:24:53 -0700

    add current state of RF.lean file

--- CMCM/Rf.lean
+++ CMCM/Rf.lean
@@ -30,14 +30,45 @@ inductive CompoundProtocol.clusterDirGlobalLin (shimAxioms : ShimAxioms n) (b : 
   (cdir_request_gcache : Behaviour.Shim.ClusterToGlobal.noPerms.linearizationEvent n shimAxioms b init e_cdir e_glin)
   : CompoundProtocol.clusterDirGlobalLin shimAxioms b init e_cdir e_glin hcdir_is_dir
 
-structure Event.isClusterCache (e_dir : Event n) : Prop where
-  dirAtDir : e_dir.isCacheEvent
-  dirCluster : e_dir.protocol = .cluster1 ∨ e_dir.protocol = .cluster2
+-- TODO: this needs to be re-written to so the global_dir (GDE) is either encapsulated (already represented) or
+-- corresponds to the prior global cache event `e_pred_gcache` that got permissions (not yet represented).
+-- Implement a def for "globalDirEventOfRequest" that captures both cases (GDE is either encapsulated, or the prior global cache event that got perms).
+-- Remember to use existing defs like
+-- "Behaviour.Shim.ClusterToGlobal" for both cases, to get to handling both cases. (the inductive constructor cases are these two cases)
+-- "dirAccessOfRequest" for the second case (use on latest e_gdir corresponding to ClusterLE cdir)
+-- In the second case, remember the global protocol uses coherent read and writes, to get `e_pred_gcache`'s directory event (GDE).
+-- But please be thorough in re-using existing defs and lemmas, to make the proof easier.
+def Behaviour.Shim.ClusterToGlobal.globalCacheAccessWithDir
+  (b : Behaviour n) (init : InitialSystemState n) (e_cdir : Event n)
+  (hshim : Behaviour.Shim.ClusterToGlobal n b init e_cdir) (e_gdir : Event n) : Prop :=
+  match hshim with
+  | .encapGlobalCache _ _ =>
+    ∃ e_gcache ∈ b,
+      Event.clusterDirEncapCorrespondingGlobalCache n b e_cdir e_gcache ∧
+      b.dirAccessOfRequest n init e_gcache e_gdir
+  | .noGlobalCache _ _ =>
+    ∃ e_gcache ∈ b,
+      Event.clusterDirEncapsulatesGlobalCacheWithPerms n b init e_cdir e_gcache ∧
+      b.dirAccessOfRequest n init e_gcache e_gdir
+
+/-
+inductive CompoundProtocol.globalDirEventOfClusterDir (cmp : CompoundProtocol n) (b : Behaviour n) (init : InitialSystemState n)
+  (e_cdir e_gdir : Event n) : Prop
+| viaShim
+  (hcdir_is_dir : e_cdir.isDirectoryEvent n)
+  (hshim : Behaviour.Shim.ClusterToGlobal n b init e_cdir)
+  (haccess : Behaviour.Shim.ClusterToGlobal.globalCacheAccessWithDir n b init e_cdir hshim e_gdir)
+  : CompoundProtocol.globalDirEventOfClusterDir cmp b init e_cdir e_gdir
+-/
 
 structure CompoundProtocol.globalLinearizationEventOfRequest (cmp : CompoundProtocol n) (b : Behaviour n) (init : InitialSystemState n)
   (e_creq : Event n) (hcreq_cluster : e_creq.isClusterCache) : Prop where
   hreq's_dir_access : ∃ e_cdir ∈ b, b.dirAccessOfRequest n init e_creq e_cdir
-  hreq's_global_lin: ∃ e_gdir ∈ b, CompoundProtocol.clusterDirGlobalLin n cmp.shimAxioms b init hreq's_dir_access.choose e_gdir (Behaviour.dirAccessOfRequest.isDirEvent n hreq's_dir_access.choose_spec.right)
+  -- It's the hreq's_global_dir below that needs to be re-written as per the above TODO.
+  hreq's_global_lin : ∃ e_gdir ∈ b,
+    Behaviour.Shim.ClusterToGlobal.globalCacheAccessWithDir n b init hreq's_dir_access.choose
+    (cmp.shimAxioms.clusterToGlobal b init hreq's_dir_access.choose hreq's_dir_access.choose_spec.right.isDirEvent)
+    e_gdir
 
 -- Try to see if this TODO is provable...
 
@@ -53,107 +84,107 @@ structure Behaviour.readsFrom.gleImmediatelyBefore (cmp : CompoundProtocol n) (b
     ¬ (hw_gle.hreq's_global_lin.choose.OrderedBefore n hinter_gle.hreq's_global_lin.choose ∧
        hinter_gle.hreq's_global_lin.choose.OrderedBefore n hr_gle.hreq's_global_lin.choose)
 
+/- is it possible to prove that if there's a cluster directory event `e_cdir`,
+then there must be a cluster cache request event `e_creq` that encapsulates `e_cdir` and corresponds to `e_cdir`? -/
+
+/-
+NOTES:
+- Let the `e_w` gle orderedBefore `e_r` gle be a precondition for rf of `e_w` and `e_r`.
+(rule out `e_r` gle before `e_w` gle, and intermediate writes that break the rf relation)
+-
+-/
+
+
 /-- Helper: downgrade immediately after write at the same cache -/
-structure Behaviour.readsFrom.downgradeImmediatelyAfterWrite (b : Behaviour n)
+structure Behaviour.readsFrom.downgradeAfterLatestWrite (b : Behaviour n)
   (e_w e_r : Event n) : Prop where
-  down_exists : ∃ e_down ∈ b, e_r.Encapsulates n e_down ∧ e_down.down ∧ e_down.cid = e_w.cid
-  down_immediately_after : ∃ e_down ∈ b, e_r.Encapsulates n e_down ∧ e_w.OrderedBefore n e_down ∧
-    ∀ e_inter ∈ b, e_inter.cid = e_w.cid →
-    ¬ (e_w.OrderedBefore n e_inter ∧ e_inter.OrderedBefore n e_down)
+  downgradeExistsAfter : ∃ e_down ∈ b, e_r.Encapsulates n e_down ∧ e_down.down ∧ e_down.cid = e_w.cid ∧ e_w.OrderedBefore n e_down
+    -- ∀ e_inter ∈ b, e_inter.cid = e_w.cid →
+    -- ¬ (e_inter.isWrite ∧ e_inter.OrderedBetween n e_w e_down)
 
 /-- Case 1: Write with coherent permissions (has predecessor that got perms) -/
 structure Behaviour.readsFrom.writeWithCoherentPred (cmp : CompoundProtocol n) (b : Behaviour n)
   (init : InitialSystemState n) (e_w e_r : Event n)
   (hw_cluster : e_w.isClusterCache) (hr_cluster : e_r.isClusterCache) : Prop where
   w_is_write : e_w.isWrite
+  w_not_down : ¬ e_w.down
   r_is_read : e_r.isRead
+  r_not_down : ¬ e_r.down
   w_has_perms : b.reqHasPerms n init e_w
   w_has_pred : b.reqHasPermsSoDirPred n init e_w
   gle_ordering : Behaviour.readsFrom.gleImmediatelyBefore n cmp b init e_w e_r hw_cluster hr_cluster
-  downgrade : Behaviour.readsFrom.downgradeImmediatelyAfterWrite n b e_w e_r
+  downgrade : Behaviour.readsFrom.downgradeAfterLatestWrite n b e_w e_r
 
-/-- Case 2: Coherent write request -/
-structure Behaviour.readsFrom.coherentWriteRequest (cmp : CompoundProtocol n) (b : Behaviour n)
+/-- Case 2: Write without permissions -/
+structure Behaviour.readsFrom.writeNoPerms (cmp : CompoundProtocol n) (b : Behaviour n)
   (init : InitialSystemState n) (e_w e_r : Event n)
   (hw_cluster : e_w.isClusterCache) (hr_cluster : e_r.isClusterCache) : Prop where
   w_is_write : e_w.isWrite
+  w_not_down : ¬ e_w.down
   r_is_read : e_r.isRead
-  w_is_coherent : e_w.isCoherent
+  r_not_down : ¬ e_r.down
+  w_no_perms : b.reqMissingPerms n init e_w
   gle_ordering : Behaviour.readsFrom.gleImmediatelyBefore n cmp b init e_w e_r hw_cluster hr_cluster
-  downgrade : Behaviour.readsFrom.downgradeImmediatelyAfterWrite n b e_w e_r
+  downgrade_to_gcache :
+    ∃ e_gcache ∈ b, (gle_ordering.hw_gle.hreq's_dir_access.choose).clusterDirEncapCorrespondingGlobalCache n b e_gcache ∧
+      Behaviour.readsFrom.downgradeAfterLatestWrite n b e_gcache e_r
 
-/-- Case 3: Non-coherent write without permissions -/
-structure Behaviour.readsFrom.nonCoherentWrite (cmp : CompoundProtocol n) (b : Behaviour n)
+-- TODO: udpated to cover the "`e_w` has a ClusterLE" ordered after itself.
+/-- Case 2: Write without permissions -/
+structure Behaviour.readsFrom.writeNoPerms (cmp : CompoundProtocol n) (b : Behaviour n)
   (init : InitialSystemState n) (e_w e_r : Event n)
   (hw_cluster : e_w.isClusterCache) (hr_cluster : e_r.isClusterCache) : Prop where
   w_is_write : e_w.isWrite
+  w_not_down : ¬ e_w.down
   r_is_read : e_r.isRead
-  w_non_coherent : ¬ e_w.isCoherent
+  r_not_down : ¬ e_r.down
   w_no_perms : b.reqMissingPerms n init e_w
   gle_ordering : Behaviour.readsFrom.gleImmediatelyBefore n cmp b init e_w e_r hw_cluster hr_cluster
   downgrade_to_gcache : ∃ e_down ∈ b, e_r.Encapsulates n e_down ∧ e_down.down ∧
-    e_down.protocol = .global ∧ e_down.isGlobalCache
+    e_down.protocol = .global ∧ e_down.isGlobalCache ∧
+    ∃ e_gcache ∈ b, (gle_ordering.hw_gle.hreq's_dir_access.choose).clusterDirEncapCorrespondingGlobalCache n b e_gcache ∧
+      e_gcache.OrderedBefore n e_down
 
 /-- The Rf (reads-from) relation between a write request and a read request.
     The write's GLE must be immediately before the read's GLE (no intermediate GLE). -/
-inductive Behaviour.readsFrom (shimAxioms : ShimAxioms n) (cmp : CompoundProtocol n) (b : Behaviour n)
+inductive Behaviour.readsFrom.diffCluster (cmp : CompoundProtocol n) (b : Behaviour n)
   (init : InitialSystemState n) (e_w e_r : Event n)
-  (hw_cluster : e_w.isClusterCache) (hr_cluster : e_r.isClusterCache) : Prop
+  (hw_cluster : e_w.isClusterCache) (hr_cluster : e_r.isClusterCache)
+  (hdiff_cluster : e_w.protocol ≠ e_r.protocol) : Prop
 | writeWithCoherentPerms
   (h : Behaviour.readsFrom.writeWithCoherentPred n cmp b init e_w e_r hw_cluster hr_cluster)
-  : Behaviour.readsFrom shimAxioms cmp b init e_w e_r hw_cluster hr_cluster
-| coherentWriteRequestNoPerms
-  (h : Behaviour.readsFrom.coherentWriteRequest n cmp b init e_w e_r hw_cluster hr_cluster)
-  : Behaviour.readsFrom shimAxioms cmp b init e_w e_r hw_cluster hr_cluster
-| nonCoherentWriteNoPerms
-  (h : Behaviour.readsFrom.nonCoherentWrite n cmp b init e_w e_r hw_cluster hr_cluster)
-  : Behaviour.readsFrom shimAxioms cmp b init e_w e_r hw_cluster hr_cluster
+  : Behaviour.readsFrom.diffCluster cmp b init e_w e_r hw_cluster hr_cluster hdiff_cluster
+| writeNoCoherentPerms
+  (h : Behaviour.readsFrom.writeNoPerms n cmp b init e_w e_r hw_cluster hr_cluster)
+  : Behaviour.readsFrom.diffCluster cmp b init e_w e_r hw_cluster hr_cluster hdiff_cluster
+
+inductive Behaviour.readsFrom.sameCluster (cmp : CompoundProtocol n) (b : Behaviour n)
+  (init : InitialSystemState n) (e_w e_r : Event n)
+  (hw_cluster : e_w.isClusterCache) (hr_cluster : e_r.isClusterCache)
+  (hsame_cluster : e_w.protocol = e_r.protocol) : Prop
+|
 
 /- -- TODO (Attempted above.):
 Define the rf relation between a write request `e_w` and a read request `e_r`:
 assume the "globalLinearizationEventOfRequest" (GLE) of the write request `e_w_gle` is immediately before
 the GLE of the read request `e_r_gle` (there is no intermediate GLE between them).
 Define cases for the rf relation:
-1. If `e_w_gle`
+1. If `e_w`
   has coherent permissions (hits in cache, there's a predecessor req that gets perms for `e_w_gle`),
   then there exists a downgrade event `e_down` encapsulated by `e_r` (and `e_r_gle`) to `e_w`.
-  This `e_down` is immediately after `e_w` (there is no intermediate cache event between `e_w` and `e_down`, at `e_w`'s cache)
-2. If `e_w_gle`'s stateAfter is coherent (`e_w_gle` is a coherent request!) the there must exist a downgrade `e_down`
-  that satisfies the same conditions as above. (`e_r` downgrades `e_w`'s coherent permissions)
-3. If `e_w_gle` is a non-coherent request, and does not have coherent permissions then `e_r`
-  encapsulates a downgrade `e_down` to `e_w`'s global cache permissions.
+  This `e_down` is after `e_w` (and there is no other write event between `e_w` and `e_down`, at `e_w`'s cache)
+2. If `e_w` has no coherent permissions, then there exists a downgrade event `e_down` encapsulated by `e_r` (and `e_r_gle`) to `e_w`'s global cache permissions.
+  This `e_down` is after `e_w` (and there is no other write event between `e_w` and `e_down`, at `e_w`'s cache).
+  Moreover, there exists a global cache event `e_gcache` that corresponds to the directory event that either gets global permissions from the global directory
+  to `e_w`,
+  or is just a global cache access request (already has permissions).
 -/
 
-/-! # Global Linearization Event (GLE) and Rf (Reads-From) Definitions
-
-This file defines:
-1. The Global Linearization Event (GLE) of a request
-2. The Rf (reads-from) relation between write and read requests
-3. A theorem proving the Rf relation holds
-
-## Global Linearization Event (GLE)
-
-The GLE is the global directory access event corresponding to a request `e_req`.
-It relates `e_req` to the global directory linearization event that linearizes it globally.
-
-The definition follows the linearization hierarchy:
-- First get the request's linearization event (cache or directory)
-- If it linearizes in cache: trace back to the preceding cluster directory that obtained permissions,
-  then find that directory's global linearization event
-- If it linearizes in cluster directory: find that directory's global linearization event
-  - If the cluster directory has global cache permissions: the GLE comes from the global cache's linearization
-  - If not: the cluster directory encapsulates a global cache request whose linearization is the GLE
-- Similarly for global protocol events (cache vs directory linearization)
-
+/-
+Cases to think about in the proof below.
+1. If if `e_w` and `e_r` are in the same cluster.
 -/
 
-/- The Global Linearization Event (GLE) of a request `e_req`.
-This follows the compound linearization infrastructure to trace through the hierarchy:
-request → cluster linearization → compound linearization → cluster directory → global linearization → GLE
-
-We build up from cmp.compoundLinearizationEvent which gives us the compound linearization event.
-If it's a clusterCacheLin, we trace back to the predecessor directory that got permissions.
-If it's a clusterDirLin, we extract the directory and handle the clusterDirectoryLinearizationEvent cases:
-  - If gcache already has perms: GLE is the predecessor directory that gave gcache perms
-  - If gcache doesn't have perms: GLE is the encapsulated global directory access
--/
+-- theorem Behaviour.readFrom_holds
+--   : Behaviour.readsFrom  := by
+--   sorry
