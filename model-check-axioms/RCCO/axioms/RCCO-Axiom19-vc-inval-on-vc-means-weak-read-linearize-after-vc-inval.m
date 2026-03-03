951cfb2c391f3a7af461233f10107c9d27b6eb3a
installer <installer@gmail.com>
2026-03-03 15:10:26 -0700

    use RCC-O axiom 15, 17, 19 use 2 addresses 2 caches

--- model-check-axioms/RCCO/axioms/RCCO-Axiom19-vc-inval-on-vc-means-weak-read-linearize-after-vc-inval.m
+++ model-check-axioms/RCCO/axioms/RCCO-Axiom19-vc-inval-on-vc-means-weak-read-linearize-after-vc-inval.m
@@ -38,7 +38,7 @@
     U_NET_MAX: 12;
   
   ---- SSP declaration constants
-    NrCachesL1C1: 4;
+    NrCachesL1C1: 2;
   
 --Backend/Murphi/MurphiModular/GenTypes
   type
@@ -115,7 +115,7 @@
       
     ----Backend/Murphi/MurphiModular/Types/GenMachineSets
       -- Cluster: C1
-      OBJSET_cacheL1C1: scalarset(3);
+      OBJSET_cacheL1C1: scalarset(2);
       OBJSET_directoryL1C1: enum{directoryL1C1};
       C1Machines: union{OBJSET_cacheL1C1, OBJSET_directoryL1C1};
       
