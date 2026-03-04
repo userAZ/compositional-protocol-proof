36a1e1b45feaf8e0b01fc609cdc6f41e62a29780
installer <installer@gmail.com>
2026-03-03 17:37:30 -0700

    commit state for leanblueprint

--- CompositionalProtocolProof.lean
+++ CompositionalProtocolProof.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `CompositionalProtocolProof` library.
 -- Import modules here that should be built as part of the library.
-import CompositionalProtocolProof.Basic
+import CompositionalProtocolProof.CompoundPPOs
