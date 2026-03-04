af7beee2e1fa599bab7dab7f5026d24d189e21cc
installer <installer@gmail.com>
2026-03-03 18:49:31 -0700

    add nix shell file for leanblueprint

---
+++ leanblueprint-shell.nix
@@ -0,0 +1,21 @@
+let
+
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+in
+
+
+pkgs.mkShellNoCC {
+
+  packages = with pkgs; [
+    graphviz
+    python312Packages.plastexdepgraph
+    python312Packages.leanblueprint
+    texliveFull
+    texlivePackages.latexmk
+  ];
+
+}
+
