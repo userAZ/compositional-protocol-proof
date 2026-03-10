dce4a0070d3250d3f513485e0e95a84d50e072e1
installer <installer@gmail.com>
2026-03-10 16:00:35 -0600

    add CMCM RfTheorem.lean to the build -- wasn't building it previously.

--- CompositionalProtocolProof.lean
+++ CompositionalProtocolProof.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `CompositionalProtocolProof` library.
 -- Import modules here that should be built as part of the library.
 import CompositionalProtocolProof.CompoundPPOs
+import CompositionalProtocolProof.CompositionalMCM
