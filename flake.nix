8e6f6eb154a09d77ca554c21807a9af55f765fb4
installer <installer@gmail.com>
2026-03-11 16:11:59 -0600

    nix flake for leanblueprint

--- flake.nix
+++ flake.nix
@@ -5,18 +5,30 @@
 
   outputs = { self, nixpkgs }:
   let
-    system = builtins.currentSystem;
-    pkgs = import nixpkgs { inherit system; };
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    mkShellFor = system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          graphviz
+          python312Packages.plastexdepgraph
+          python312Packages.leanblueprint
+          texliveFull
+          texlivePackages.latexmk
+        ];
+      };
   in
   {
-    devShells.${system}.default = pkgs.mkShellNoCC {
-      packages = with pkgs; [
-        graphviz
-        python312Packages.plastexdepgraph
-        python312Packages.leanblueprint
-        texliveFull
-        texlivePackages.latexmk
-      ];
-    };
+    devShells = builtins.listToAttrs (map (system: {
+      name = system;
+      value = { default = mkShellFor system; };
+    }) systems);
   };
 }
