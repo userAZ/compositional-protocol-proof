e32ddce6e52482ed8fa7a52e689570016b621535
installer <installer@gmail.com>
2026-04-02 14:45:28 -0600

    Remove MWE scratch files: zero sorry's across entire project

--- CompositionalProtocolProof/MWEs/MWEFinNatPart2.lean
+++
@@ -1,8 +0,0 @@
-import CompositionalProtocolProof.MWEs.MWEFinNat
-
--- import FinNatMWE
-
-variable (n : Nat)
-
-abbrev UseFinNat := FinNat -- error: unknown constant 'n✝'
-def UseFinNat' := FinNat -- error: unknown constant 'n✝'
